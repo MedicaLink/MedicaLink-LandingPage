6118e1e63d301ba3790167663b9c061f7cfd11c3
chirangashalitha <chiranga.20244008@iit.ac.lk>
2024-12-04 18:24:45 +0530

    Change homepage layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,3 @@ openMenuButton.addEventListener("click", (e) => {
 sideBarCloseButton.addEventListener("click", (e) => {
   document.getElementById("sidebar").classList.add("hidden");
 });
-
-
-function isMobile() {
-  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  return /android/i.test(userAgent) || /iPhone|iPad|iPod/i.test(userAgent);
-}
-
-if (isMobile()) {
-  // Perform actions for mobile devices
-  document.body.classList.add('is-mobile');
-} else {
-  // Perform actions for non-mobile devices
-  document.body.classList.add('is-desktop');
-}
