62033a16d2903466764d20de6365216e569429d1
randevmedicalink <randev.medicalink@gmail.com>
2025-01-03 18:14:22 +0530

    trying hard

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function sendMail() {
     //   });
 
       var templateParams = {
-        name: document.getElementById("name").value,
+      name: document.getElementById("name").value,
       email: document.getElementById("email").value,
       phone: document.getElementById("phone").value,
       message: document.getElementById("message").value,
@@ -44,9 +44,11 @@ function sendMail() {
       
       emailjs.send('service_xbpfvvr', 'template_arhnh7m', templateParams).then(
         (response) => {
+        alert("Success!");
           console.log('SUCCESS!', response.status, response.text);
         },
         (error) => {
+            alert("Error");
           console.log('FAILED...', error);
         },
       );
