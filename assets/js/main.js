3b240d685ac681a6da1e93fc6b7a67170f606c7e
randevmedicalink <randev.medicalink@gmail.com>
2025-01-05 20:23:31 +0530

    desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,12 @@ openMenuButton.addEventListener("click", (e) => {
 sideBarCloseButton.addEventListener("click", (e) => {
   document.getElementById("sidebar").classList.add("hidden");
 });
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    // After 6 seconds, show and trigger fade-up
+    document.getElementById('get-started-btn').style.display = 'inline-block';
+    AOS.init(); // Re-initialize AOS
+  }, 6000); // Adjust this value based on your animation's duration
+});
+
