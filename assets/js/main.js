3b6dc2bc29829be93225eaeaf6ce38efd8c8414e
randevmedicalink <randev.medicalink@gmail.com>
2025-01-05 21:44:13 +0530

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,24 @@ sideBarCloseButton.addEventListener("click", (e) => {
   document.getElementById("sidebar").classList.add("hidden");
 });
 
-window.addEventListener('load', function() {
-  setTimeout(function() {
-    // After 6 seconds, show and trigger fade-up
-    document.getElementById('get-started-btn').style.display = 'inline-block';
-    AOS.init(); // Re-initialize AOS
-  }, 6000); // Adjust this value based on your animation's duration
-});
 
+// mobile typewriter
+window.addEventListener('load', function() {
+  // Hide mobile version for larger screens
+  if (window.innerWidth <= 768) {
+    document.querySelector('.desktop-version').style.display = 'none';  // Hide desktop version
+    document.querySelector('.mobile-version').style.display = 'block';  // Show mobile version
+    setTimeout(function() {
+      document.getElementById('get-started-btn-mobile').style.display = 'inline-block'; // Show the button
+      AOS.init();  // Initialize AOS for mobile version
+    }, 11000);
+  } else {
+    // Hide mobile version for larger screens
+    document.querySelector('.desktop-version').style.display = 'block';  // Show desktop version
+    document.querySelector('.mobile-version').style.display = 'none';  // Hide mobile version
+    setTimeout(function() {
+      document.getElementById('get-started-btn').style.display = 'inline-block'; // Show the button
+      AOS.init();  // Initialize AOS for desktop version
+    }, 6000);
+  }
+});
