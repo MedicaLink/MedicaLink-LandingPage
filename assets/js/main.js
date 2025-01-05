1f971e41e862926fb23844cdb32a269a333f9247
randevmedicalink <randev.medicalink@gmail.com>
2025-01-05 23:00:29 +0530

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,23 +10,4 @@ sideBarCloseButton.addEventListener("click", (e) => {
 });
 
 
-// mobile typewriter
-window.addEventListener('load', function() {
-  // Hide mobile version for larger screens
-  if (window.innerWidth <= 768) {
-    document.querySelector('.desktop-version').style.display = 'none';  // Hide desktop version
-    document.querySelector('.mobile-version').style.display = 'block';  // Show mobile version
-    setTimeout(function() {
-      document.getElementById('get-started-btn-mobile').style.display = 'inline-block'; // Show the button
-      AOS.init();  // Initialize AOS for mobile version
-    }, 11000);
-  } else {
-    // Hide mobile version for larger screens
-    document.querySelector('.desktop-version').style.display = 'block';  // Show desktop version
-    document.querySelector('.mobile-version').style.display = 'none';  // Hide mobile version
-    setTimeout(function() {
-      document.getElementById('get-started-btn').style.display = 'inline-block'; // Show the button
-      AOS.init();  // Initialize AOS for desktop version
-    }, 6000);
-  }
-});
+
