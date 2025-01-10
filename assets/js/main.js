6322086efb7477e2c9d7455e4b272c74ed83e368
randevmedicalink <randev.medicalink@gmail.com>
2025-01-11 00:29:35 +0530

    vision and mission

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,16 @@ sideBarCloseButton.addEventListener("click", (e) => {
   document.getElementById("sidebar").classList.add("hidden");
 });
 
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Adjust based on your project structure
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Add Poppins font
+      },
+    },
+  },
+  plugins: [],
+};
 
 
