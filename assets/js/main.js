904f4022ba8e3a8536f0a596989cb661b4e9f498
randevmedicalink <randev.medicalink@gmail.com>
2025-01-22 04:05:30 +0530

    slider trial

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,34 @@ module.exports = {
 };
 
 
+const carousel = document.querySelector('#carouselExample');
+  let startX, endX;
+
+  carousel.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX; // Get the starting X coordinate
+  });
+
+  carousel.addEventListener('touchmove', (e) => {
+    endX = e.touches[0].clientX; // Update the X coordinate as the user drags
+  });
+
+  carousel.addEventListener('touchend', () => {
+    if (startX && endX) {
+      const diff = startX - endX;
+
+      // If the swipe is more than 50px, trigger slide transition
+      if (Math.abs(diff) > 50) {
+        if (diff > 0) {
+          // Swipe left, go to the next slide
+          $('#carouselExample').carousel('next');
+        } else {
+          // Swipe right, go to the previous slide
+          $('#carouselExample').carousel('prev');
+        }
+      }
+    }
+
+    // Reset the variables
+    startX = null;
+    endX = null;
+  });
